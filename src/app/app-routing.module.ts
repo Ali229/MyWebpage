cf6d7bdf3b1f62c716015a97ac19466f48bc8c54
Muhammad Ali <alinaeem229@gmail.com>
2019-03-07 19:53:22 -0500

    Fixed navbar & added more pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {ProjectsComponent} from "./projects/projects.component";
+import {EmploymentComponent} from "./employment/employment.component";
+import {EducationComponent} from "./education/education.component";
+import {ContactComponent} from "./contact/contact.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'employment', component: EmploymentComponent},
+  {path: 'education', component: EducationComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '**', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import {MDBBootstrapModule} from 'angular-bootstrap-md';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {NavbarComponent} from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { EducationComponent } from './education/education.component';
+import { EmploymentComponent } from './employment/employment.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    EducationComponent,
+    EmploymentComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
