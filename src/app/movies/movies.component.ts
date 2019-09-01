033de0d2a03e4a782f2105f6500cec4ab5cb566e
Muhammad Ali <alinaeem229@gmail.com>
2019-08-31 22:24:52 -0400

    Error checking

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -36,57 +36,59 @@ export class MoviesComponent implements OnInit {
         this.scoreCount = 0;
         this.overallScore = 0;
         this.data = info;
-        if (this.data.Metascore !== 'N/A') {
-          if (this.data.Metascore > 79) {
-            this.metaColor = 'success';
-          } else if (this.data.Metascore > 49) {
-            this.metaColor = 'warning';
-          } else if (this.data.Metascore <= 49) {
-            this.metaColor = 'danger';
+        if (this.data.Response !== 'False') {
+          if (this.data.Metascore !== 'N/A') {
+            if (this.data.Metascore > 79) {
+              this.metaColor = 'success';
+            } else if (this.data.Metascore > 49) {
+              this.metaColor = 'warning';
+            } else if (this.data.Metascore <= 49) {
+              this.metaColor = 'danger';
+            }
+            this.totalScore += Number(this.data.Metascore);
+            this.scoreCount++;
           }
-          this.totalScore += Number(this.data.Metascore);
-          this.scoreCount++;
-        }
-        if (this.data.imdbRating !== 'N/A') {
-          this.data.imdbRating = this.data.imdbRating * 10;
-          if (this.data.imdbRating > 79) {
-            this.imdbColor = 'success';
-          } else if (this.data.imdbRating > 49) {
-            this.imdbColor = 'warning';
-          } else if (this.data.imdbRating <= 49) {
-            this.imdbColor = 'danger';
+          if (this.data.imdbRating !== 'N/A') {
+            this.data.imdbRating = this.data.imdbRating * 10;
+            if (this.data.imdbRating > 79) {
+              this.imdbColor = 'success';
+            } else if (this.data.imdbRating > 49) {
+              this.imdbColor = 'warning';
+            } else if (this.data.imdbRating <= 49) {
+              this.imdbColor = 'danger';
+            }
+            this.totalScore += Number(this.data.imdbRating);
+            this.scoreCount++;
           }
-          this.totalScore += Number(this.data.imdbRating);
-          this.scoreCount++;
-        }
-        if (this.data.Ratings.length > 0) {
-          // tslint:disable-next-line:prefer-for-of
-          for (let x = 0; x < this.data.Ratings.length; x++) {
-            if (this.data.Ratings[x].Source === 'Rotten Tomatoes') {
-              this.rottenScore = Number(this.data.Ratings[x].Value.replace('%', ''));
-              if (this.rottenScore > 79) {
-                this.rottenImage = 'tomato_full.png';
-                this.rottenColor = 'success';
-              } else if (this.rottenScore > 49) {
-                this.rottenImage = 'tomato_full.png';
-                this.rottenColor = 'warning';
-              } else if (this.rottenScore <= 49) {
-                this.rottenImage = 'tomato_rotten.png';
-                this.rottenColor = 'danger';
+          if (this.data.Ratings.length > 0) {
+            // tslint:disable-next-line:prefer-for-of
+            for (let x = 0; x < this.data.Ratings.length; x++) {
+              if (this.data.Ratings[x].Source === 'Rotten Tomatoes') {
+                this.rottenScore = Number(this.data.Ratings[x].Value.replace('%', ''));
+                if (this.rottenScore > 79) {
+                  this.rottenImage = 'tomato_full.png';
+                  this.rottenColor = 'success';
+                } else if (this.rottenScore > 49) {
+                  this.rottenImage = 'tomato_full.png';
+                  this.rottenColor = 'warning';
+                } else if (this.rottenScore <= 49) {
+                  this.rottenImage = 'tomato_rotten.png';
+                  this.rottenColor = 'danger';
+                }
+                this.totalScore += Number(this.rottenScore);
+                this.scoreCount++;
               }
-              this.totalScore += Number(this.rottenScore);
-              this.scoreCount++;
             }
           }
-        }
-        // tslint:disable-next-line:radix
-        this.overallScore = parseInt((this.totalScore / this.scoreCount).toFixed(2));
-        if (this.overallScore > 79) {
-          this.totalColor = 'success';
-        } else if (this.overallScore > 49) {
-          this.totalColor = 'warning';
-        } else if (this.overallScore <= 49) {
-          this.totalColor = 'danger';
+          // tslint:disable-next-line:radix
+          this.overallScore = parseInt((this.totalScore / this.scoreCount).toFixed(2));
+          if (this.overallScore > 79) {
+            this.totalColor = 'success';
+          } else if (this.overallScore > 49) {
+            this.totalColor = 'warning';
+          } else if (this.overallScore <= 49) {
+            this.totalColor = 'danger';
+          }
         }
         this.loading = false;
       });
