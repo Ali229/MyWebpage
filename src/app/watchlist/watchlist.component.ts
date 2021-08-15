85890d9671018e760915a6dfc77ccf776bb76c7b
Muhammad Ali <alinaeem229@gmail.com>
2021-08-15 17:30:38 -0400

    Add filter for watchlist

--- src/app/watchlist/watchlist.component.ts
+++ src/app/watchlist/watchlist.component.ts
@@ -1,17 +1,35 @@
 import {Component, OnInit} from '@angular/core';
 import {TitleService} from '../services/title.service';
 import {AuthService} from '../services/auth.service';
+import {Title} from '../models/title.model';
 
 @Component({
   selector: 'app-watchlist',
   templateUrl: './watchlist.component.html',
   styleUrls: ['./watchlist.component.scss']
 })
-export class WatchlistComponent implements OnInit {
+export class WatchlistComponent {
+  selectedType = 'All';
+  filteredList: Title[] = null;
 
   constructor(public auth: AuthService, public ts: TitleService) {
+    this.changeMediaType(this.selectedType);
   }
 
-  ngOnInit() {
+  async remove(id) {
+   await this.auth.removeFromWatchlist(id);
+   this.changeMediaType(this.selectedType);
+  }
+
+  changeMediaType(selectedType) {
+    if (selectedType === 'All') {
+      return this.filteredList = this.auth.watchlist;
+    }
+    this.filteredList = [];
+    for (const title of this.auth.watchlist) {
+      if (title.media_type === selectedType) {
+        this.filteredList.push(title);
+      }
+    }
   }
 }
