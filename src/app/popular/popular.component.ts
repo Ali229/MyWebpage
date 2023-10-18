ab8cb332587767b30184cd45f62822715d264ffa
Muhammad Ali <alinaeem229@gmail.com>
2023-10-18 15:46:57 -0400

    Only load current year for popular

--- src/app/popular/popular.component.ts
+++ src/app/popular/popular.component.ts
@@ -8,12 +8,13 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./popular.component.scss']
 })
 export class PopularComponent implements OnInit {
-  currentYear = new Date().getFullYear();
+  currentYear = new Date().getFullYear().toString();
   popularYears: any[] = [];
-  popularMovies: any[] = [];
+  popularMovies: any = {};
 
   constructor(private http: HttpClient, public ts: TitleService) {
   }
+
   ngOnInit() {
     this.initializeYears();
     this.fetchMostPopular(this.currentYear);
@@ -21,35 +22,35 @@ export class PopularComponent implements OnInit {
 
   initializeYears() {
     for (let i = 0; i < 5; i++) {
-      const year = (this.currentYear - i).toString();
-      this.popularYears.push({ year, index: i, isSelected: i === 0 });
+      const year = (parseInt(this.currentYear, 10) - i).toString();
+      this.popularYears.push({year, index: i, isSelected: i === 0});
+      this.popularMovies[year] = []; // Initialize movies array for each year
     }
   }
 
+  toggleYear(selectedYear: string) {
+    this.popularYears.forEach((yearData) => {
+      yearData.isSelected = yearData.year === selectedYear;
+    });
 
-  fetchMostPopular(year: number) {
+    if (this.popularMovies[selectedYear].length === 0) {
+      this.fetchMostPopular(selectedYear);
+    }
+  }
+
+  fetchMostPopular(year: string) {
     const apiKey = 'e84ac8af3c49ad3253e0369ec64dfbff';
     const apiUrl = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc` +
       `&api_key=${apiKey}&primary_release_year=${year}`;
 
     this.http.get(apiUrl).subscribe((response: any) => {
-      this.popularMovies.push(response);
-      if (year > this.currentYear - 4) {
-        this.fetchMostPopular(year - 1);
-      }
+      this.popularMovies[year] = response;
     });
   }
-
-  toggleYear(selectedYear: string) {
-    this.popularYears.forEach(yearData => {
-      yearData.isSelected = yearData.year === selectedYear;
-    });
-  }
-
   scrollToElement() {
     const target = document.getElementById('target');
     if (target) {
-      target.scrollIntoView({ behavior: 'smooth' });
+      target.scrollIntoView({behavior: 'smooth'});
     }
   }
 }
