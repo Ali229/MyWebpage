4ac50e1c43800c5177a4118951c39616ac0b545b
Muhammad Ali <alinaeem229@gmail.com>
2023-10-18 08:46:47 -0400

    Move popular out of movies component

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,55 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { TitleService } from '../services/title.service';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.scss']
 })
-export class MoviesComponent implements OnInit {
-  currentYear = new Date().getFullYear();
-  popularYears: any[] = [];
-  popularMovies: any[] = [];
-
-  constructor(private http: HttpClient, public ts: TitleService) { }
-
-  ngOnInit() {
-    this.initializeYears();
-    this.fetchMostPopular(this.currentYear);
-  }
-
-  initializeYears() {
-    for (let i = 0; i < 5; i++) {
-      const year = (this.currentYear - i).toString();
-      this.popularYears.push({ year, index: i, isSelected: i === 0 });
-    }
-  }
-
-
-  fetchMostPopular(year: number) {
-    const apiKey = 'e84ac8af3c49ad3253e0369ec64dfbff';
-    const apiUrl = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc` +
-      `&api_key=${apiKey}&primary_release_year=${year}`;
-
-    this.http.get(apiUrl).subscribe((response: any) => {
-      this.popularMovies.push(response);
-      if (year > this.currentYear - 4) {
-        this.fetchMostPopular(year - 1);
-      }
-    });
-  }
-
-  toggleYear(selectedYear: string) {
-    this.popularYears.forEach(yearData => {
-      yearData.isSelected = yearData.year === selectedYear;
-    });
-  }
-
-  scrollToElement() {
-    const target = document.getElementById('target');
-    if (target) {
-      target.scrollIntoView({ behavior: 'smooth' });
-    }
-  }
+export class MoviesComponent {
+  constructor(public ts: TitleService) { }
 }
--- src/app/popular/popular.component.ts
+++ src/app/popular/popular.component.ts
@@ -1,14 +1,55 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {TitleService} from '../services/title.service';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-popular',
   templateUrl: './popular.component.html',
   styleUrls: ['./popular.component.scss']
 })
-export class PopularComponent {
-  @Input() public popList: any;
+export class PopularComponent implements OnInit {
+  currentYear = new Date().getFullYear();
+  popularYears: any[] = [];
+  popularMovies: any[] = [];
 
-  constructor(public ts: TitleService) {
+  constructor(private http: HttpClient, public ts: TitleService) {
+  }
+  ngOnInit() {
+    this.initializeYears();
+    this.fetchMostPopular(this.currentYear);
+  }
+
+  initializeYears() {
+    for (let i = 0; i < 5; i++) {
+      const year = (this.currentYear - i).toString();
+      this.popularYears.push({ year, index: i, isSelected: i === 0 });
+    }
+  }
+
+
+  fetchMostPopular(year: number) {
+    const apiKey = 'e84ac8af3c49ad3253e0369ec64dfbff';
+    const apiUrl = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc` +
+      `&api_key=${apiKey}&primary_release_year=${year}`;
+
+    this.http.get(apiUrl).subscribe((response: any) => {
+      this.popularMovies.push(response);
+      if (year > this.currentYear - 4) {
+        this.fetchMostPopular(year - 1);
+      }
+    });
+  }
+
+  toggleYear(selectedYear: string) {
+    this.popularYears.forEach(yearData => {
+      yearData.isSelected = yearData.year === selectedYear;
+    });
+  }
+
+  scrollToElement() {
+    const target = document.getElementById('target');
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
   }
 }
