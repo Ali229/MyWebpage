dbaab208614dd9a751250f07a8d574ba70a73f7e
Muhammad Ali <alinaeem229@gmail.com>
2023-10-23 09:34:35 -0400

    Fix app-profile calling auth service twice &  add watchlist length

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -6,6 +6,19 @@ import { TitleService } from '../services/title.service';
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.scss']
 })
-export class MoviesComponent {
-  constructor(public ts: TitleService) { }
+export class MoviesComponent implements OnInit {
+  isPhone: boolean = window.innerWidth <= 767.98; // Check if the screen width is less than or equal to your breakpoint
+
+  constructor(public ts: TitleService) {
+  }
+
+  ngOnInit() {
+    // Subscribe to window resize events to update the isPhone value
+    window.addEventListener('resize', this.onResize.bind(this));
+  }
+
+  onResize() {
+    // Update the isPhone value when the window is resized
+    this.isPhone = window.innerWidth <= 767.98;
+  }
 }
